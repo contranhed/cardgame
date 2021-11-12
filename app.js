4f6c507e069d56caae34bb8cce920ace66b697c6
Connie Tran Hedberg <connietranhedberg@gmail.com>
2021-11-12 15:38:32 +0100

    Add functionality to fetch a card but the logic for display is missing

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ const higherButton = createAndAppend('button', divButtons)
 higherButton.setAttribute('id', 'higherbutton')
 higherButton.innerText = 'Higher'
 
+const submitButton = createAndAppend('button', containerElement)
+submitButton.innerText = 'GET CARD'
+submitButton.setAttribute('id', 'submitbutton')
+
 lowerButton.addEventListener('click', () => {
     console.log('clicked lower')
 })
@@ -40,16 +44,42 @@ higherButton.addEventListener('click', () => {
     console.log('clicked higher')
 })
 
+submitButton.addEventListener('click', () => {
+    console.log('submitted')
+} )
+
 // Display card
 let deck = {}
 
 async function getDeck() {
-    const res = await fetch(
-        'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2')
-    const data = await res.json()
-    deck = data
+    try {
+        const res = await fetch(
+            'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2')
+        const data = await res.json()
+        deck = data
+    }
+    catch (e) {
+        console.log('Not successful fetch')
+        console.log(e)
+    }
     console.log(deck)
 }
-
 getDeck()
 
+const cardElement = document.getElementById('card')
+const imageElement = document.getElementById('cardImage')
+
+submitButton.addEventListener('click', async () => {
+    try {
+        const res = await fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`)
+        const data = await res.json()
+        let playingCard = data.cards[0]
+        console.log(data.cards[0])
+        cardElement.children[0].innerText = playingCard.value
+        cardElement.children[1].setAttribute('src', playingCard.image)
+        submitButton.innerText = 'TRY AGAIN'
+    } catch(e) {
+        console.log('Could not draw new card')
+        console.log(e)
+    }
+})
