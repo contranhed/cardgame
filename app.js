5047a9bfbe55f3874dc59dd1302b7c5567cd3636
Connie Tran Hedberg <connietranhedberg@gmail.com>
2021-11-13 22:14:32 +0100

    Add game functionality with JavaScript implicit type coercion

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-'use strict'
 // Grabbing the elements needed for the application and storing them in new variables
 const lowerButton = document.getElementById('lowerbutton')
 const higherButton = document.getElementById('higherbutton')
+const sameButton = document.getElementById('samebutton')
+const playButton = document.getElementById('playbutton')
 const submitButton = document.getElementById('submitbutton')
 const h1HeaderElement = document.getElementById('h1header')
 const greetingElement = document.getElementById('greeting')
@@ -11,28 +12,40 @@ const imageElement = document.getElementById('cardImage')
 
 // Variable that stores the object that represents a deck of cards
 let deck = {}
-let playingCard = {}
-let guess = ''
+let playingCard = 0
+let guessingCard = {}
+let guess = {}
 
-// EventListener when user chooses button "lower"
-lowerButton.addEventListener('click', () => {
-    lowerButton.style.backgroundColor = 'green'
-    const lower = 'false'
-    chosenButton(lower)
-    console.log(lower)
-})
+// // EventListener when user chooses button "lower"
+// lowerButton.addEventListener('click', async () => {
+//     console.log('lower')
+//     chosenButton()
+// })
 
-// EventListener when user chooses button "higher"
-higherButton.addEventListener('click', () => {
-    const higher = 'true'
-    chosenButton(higher)
-    console.log(higher)
-})
+// // EventListener when user chooses button "higher"
+// higherButton.addEventListener('click', async () => {
+//     console.log('higher')
+//     chosenButton()
+// })
   
 // EventListener when user chooses button "start game"
 submitButton.addEventListener('click', async () => {
-    checkAnswer()
+    // Get and display a card
+    getNewCard()
+ 
+    // Display/hide elements as required by the page.
+    h1HeaderElement.style.display = 'none'
+    submitButton.style.display = 'none'
+    lowerButton.style.display = 'block'
+    higherButton.style.display = 'block'
+    sameButton.style.display = 'block'
+    playButton.style.display = 'none'
+    playButton.innerText = 'play again'
 })
+
+// playButton.addEventListener('click', async () => {
+//     checkAnswer()
+// })
 
 /**
  * 
@@ -51,7 +64,7 @@ async function getDeck() {
         deck = data
         console.log('Deck was successfully retrieved!')
     }
-    catch (e) {
+    catch (e) { // Error handling
         console.log('Not successful with the fetch')
         console.log(e)
     }
@@ -68,11 +81,14 @@ async function setUpGame() {
         // Remove visibility of buttons not used on landing page
         lowerButton.style.display = 'none'
         higherButton.style.display = 'none'
+        sameButton.style.display = 'none'
+        playButton.style.display = 'none'
         messageElement.style.display = 'none'
         
         // Insert text to button
         submitButton.innerText = 'START GAME'
-    } catch (e) {
+        
+    } catch (e) { // Error handling
         console.log('Could not set up the game')
         console.log(e)
     }
@@ -82,37 +98,35 @@ async function getNewCard() {
     try {
         // Fetch the card from API
         const res = await fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`)
-        
         // Store the response (translated to JSON) in a variable
         const data = await res.json()
-        
         // Set the current card to the card value received in the API response
         playingCard = data.cards[0]
-        console.log(data.cards[0])
+        console.log('i am now in getNewCard')
+        console.log(playingCard)
         
         // Set the image attribute on the card HTML element.
         cardElement.children[1].setAttribute('src', playingCard.image)
-        console.log('1. this is ' + data.cards[0].value)
-        
-        // Display/hide elements as required by the page.
-        h1HeaderElement.style.display = 'none'
-        lowerButton.style.display = 'block'
-        higherButton.style.display = 'block'
-        submitButton.innerText = 'submit'
-        
+        console.log('this is ' + data.cards[0].value)
+    
         // Insert text in existing element.
-        greetingElement.innerText = `Will next card be lower or higher than ${playingCard.value}?`
+        greetingElement.innerText = `Will next card be the same, lower or higher than ${playingCard.value}?`
 
         // Reassign value on the the higher cards
-        if(playingCard.value > 1 && playingCard.value <= 10) {
+        // NB! Taking full advantage of implicit type cocercion in JavaScript!
+        if (playingCard.value > 1 && playingCard.value < 10) {
             cardElement.children[1].setAttribute('src', playingCard.image)
-            console.log('this is below 10')
+            console.log('this is below 11')
         } 
+        else if (playingCard.value == '10') {
+            playingCard.value = 10
+            cardElement.children[1].setAttribute('src', playingCard.image)
+            console.log('this is 10')
+        }
         else if (playingCard.value == 'JACK') {
             playingCard.value = 11
             cardElement.children[1].setAttribute('src', playingCard.image)
             console.log('this is jack')
-
         } 
         else if (playingCard.value == 'QUEEN') {
             playingCard.value = 12
@@ -132,46 +146,54 @@ async function getNewCard() {
     console.log('the sent card will be: ' + playingCard.value)
     // Return the current card object (with reassigned value if needed)
     return playingCard
-    } catch(e) {
+    } catch(e) { // Error handling
         console.log('Could not draw new card')
         console.log(e)
     }
 }
 
 /**
- * 
- * @function chosenButton checks which button that was clicked 
- * A setup because eventlisteners cannot return anything
- * 
- * @param {*} res 
- * @returns boolean
+ * @function checkAnswer Checks if the guess was right
  */
-async function chosenButton(res) {
-    try {
-        if (res === false) {
-            return('false')
-        } 
-        else if (res === true) {
-            return('true')
-        }
-    } catch (e) {
-        console.log('Could not check the chosen button')
-    }
-}
-
-async function checkAnswer() {
+async function checkAnswer(guess) {
     try {
         // Function call and store the result in a variable after the promise has been resolved.
-        const card = await getNewCard()
+        const card = playingCard
         // Asssign the value to the variable that is declared on top of the file.
-        playingCard = card
+        oldCard = card
+        console.log('i am now in checkAnswer!')
         // THIS WORKS!!
-        console.log('this is the value I am going to check ' + playingCard.value) 
-        const answer = await chosenButton()
-        // TODO: This part with the guess does not work.
-        guess = answer
-        console.log('this is the button choice: ' + answer)
-    } catch (e) {
+        const newCard = await getNewCard()
+        console.log('this is the OLD value ' + oldCard.value) 
+        // THIS WORKS!
+        guessingCard = newCard
+        console.log('this is the NEW value ' + guessingCard.value)
+        console.log('this is the old type ' + typeof(oldCard.value))
+        console.log('this is the old type ' + typeof(guessingCard.value))
+
+        // Evaluate the card values and check the correct answer
+        // WORKS!
+        let correctAnswer = ''
+        if (oldCard.value > guessingCard.value) {
+            correctAnswer = 'lower'
+        }
+        else if (oldCard.value < guessingCard.value){
+            correctAnswer = 'higher'
+
+        } else if(oldCard.value == guessingCard.value) {
+            correctAnswer = 'same'
+        }
+        console.log('the correct answer is ' + correctAnswer)
+        console.log('the guess was: ' + guess)
+    
+        // WORKS!
+        if (guess == correctAnswer) {
+            console.log('you guessed right!')
+        } else {
+            console.log('you gussed wrong')
+        }
+
+    } catch (e) { // Error handling
         console.log('Could not check the answer')
         console.log(e)
     }
